aea19b658ce9ed0871fa82f5be5c067d8663fbf9
Ercio Alendre <ercio.alendre@gmail.com>
2024-02-27 12:25:31 -0300

    FEAT: implements a retry policy for the database connection service

--- src/modules/database/prisma/prisma.service.ts
+++ src/modules/database/prisma/prisma.service.ts
@@ -1,16 +1,50 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+  Logger,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+
+let retryCounter = 0;
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  public onModuleInit(): void {
-    this.$connect();
+  private readonly retryDelay = 5000;
+  private readonly logger = new Logger(PrismaService.name);
+
+  public async onModuleInit(): Promise<void> {
+    this.logger.log('Initializing database connection...');
+
+    await this.connectWithRetry();
   }
 
-  public onModuleDestroy(): void {
-    this.$disconnect();
+  public async onModuleDestroy(): Promise<void> {
+    this.logger.log('Initializing database disconnection...');
+
+    await this.$disconnect();
+  }
+
+  private async connectWithRetry(): Promise<void> {
+    try {
+      await this.$connect();
+
+      this.logger.log('Connected to the database successfully!');
+    } catch (error) {
+      this.logger.error(`Failed to connect to the database: ${error}`);
+
+      retryCounter++;
+
+      this.logger.log(
+        `Retrying database connection in ${this.retryDelay / 1000} seconds... (${retryCounter})`,
+      );
+
+      await new Promise((resolve) => setTimeout(resolve, this.retryDelay));
+
+      await this.connectWithRetry();
+    }
   }
 }
