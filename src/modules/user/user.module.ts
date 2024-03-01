0caab216f04cd5977d742eac210794dacdea6286
Ercio Alendre <ercio.alendre@gmail.com>
2024-03-01 11:23:10 -0300

    FEAT: adds DatabaseModule and UserResolver to UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserResolver } from './resolvers/user.resolver';
+import { DatabaseModule } from '../database/database.module';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  providers: [UserResolver],
+})
 export class UserModule {}
